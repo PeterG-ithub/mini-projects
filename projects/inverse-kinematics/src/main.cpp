548fc387e434ef6df49a1018fe0701a8dbb8a4af
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-01 09:55:26 -0700

    Add FPS count on terminal
    - Add deltaTime

--- projects/inverse-kinematics/src/main.cpp
+++ projects/inverse-kinematics/src/main.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1280, 720, 32), "SFML Window", sf::Style::Default);
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+    sf::Clock clock;
+    int frameCount = 0;
+    float elapsedTime = 0.0f;
     while (window.isOpen())
     {
         sf::Event event;
@@ -15,10 +18,23 @@ int main()
                 window.close();
         }
 
-        window.clear();
+        sf::Time deltaTime = clock.restart();
+        float dt = deltaTime.asSeconds();
+
+        frameCount++;
+        elapsedTime += dt;
+        if (elapsedTime > 1.0f) {
+            std::cout << "FPS: " << frameCount/elapsedTime << std::endl;
+            frameCount = 0;
+            elapsedTime = 0.0f;
+        }
+        
+
+        window.clear(sf::Color::White);
         window.draw(shape);
         window.display();
     }
 
     return 0;
 }
+
